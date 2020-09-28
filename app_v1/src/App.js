0cb6c514618ec3a29de7b24a9030b7fd6dbf9237
ezaldeenalib <ezaldeentt@gmail.com>
2020-09-28 13:53:20 +0300

    first modefay

--- app_v1/src/App.js
+++ app_v1/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
- 
 import './App.css';
+import Person from './Person/Person';
 
 function App() {
   return (
     <div className="App">
      <h1>hello world
      </h1>
+      <Person/>
     </div>
   );
   // return React.createElement('div',{className:"App"},React.createElement('h1',null,'hello'))
---
+++ app_v1/src/Person/Person.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+
+ const person = () => <p>hello were ever you are</p>
+ 
+  
+ export default person;
